2f4e93ef1c7aa86e271f5bc9a01298c41b031a4a
n-sakuma39 <naoto.sakuma39@gmail.com>
2023-08-30 17:10:04 +0900

    修正（8/30）

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { Header } from "@/components/Header/Header";
 import { Footer } from "@/components/Footer/Footer";
 import { Sidenavi } from "@/components/Sidebar/Sidebar";
@@ -5,19 +6,27 @@ import styles from "@/components/Layout/Layout.module.scss";
 
 export const Layout = ({ children, showSidebar, category, tag, works }) => {
   return (
-    <div className={`${styles.container}`}>
-      <Header />
-      <div
-        className={`${styles.layout} ${
-          showSidebar ? styles["two-column"] : styles["one-column"]
-        }`}
-      >
-        <main className={styles.main}>{children}</main>
-        {showSidebar && (
-          <Sidenavi posts={works} category={category} tag={tag} />
-        )}
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>NAOTO SAKUMA Portfolio SITE</title>
+        <link rel="icon" href="/images/favicon.ico" />
+      </Head>
+      <div className={`${styles.container}`}>
+        <Header />
+        <div
+          className={`${styles.layout} ${
+            showSidebar ? styles["two-column"] : styles["one-column"]
+          }`}
+        >
+          <main className={styles.main}>{children}</main>
+          {showSidebar && (
+            <Sidenavi posts={works} category={category} tag={tag} />
+          )}
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </>
   );
 };
